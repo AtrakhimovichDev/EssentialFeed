9adf9a952665f938d693721db000b8f672c4675d
Andrei Atrakhimovich <atrakhimovich95@gmail.com>
2022-08-02 14:08:44 +0300

    Make some properties optional inside the FeedItem

--- EssentialFeed/FeedItem.swift
+++ EssentialFeed/FeedItem.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct FeedItem {
     let id: UUID
-    let description: String
-    let location: String
+    let description: String?
+    let location: String?
     let imageURL: URL
 }
